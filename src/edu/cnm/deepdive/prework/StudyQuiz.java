9213b7132c2641c7981150320cbfd9040dc5b492
jacchevr <jacchevr@gmail.com>
2018-01-10 03:05:48 -0700

    Seems like a good place to end the night.
    Finished adding line breaks as well as additional if statements to
    current questions.
    
    Added a few notes to refer back to and also use as reference for
    trying to identify parts of code.

--- src/edu/cnm/deepdive/prework/StudyQuiz.java
+++ src/edu/cnm/deepdive/prework/StudyQuiz.java
@@ -7,6 +7,7 @@ public class StudyQuiz {
     public static void main(String[] args)
     {
 
+      //These are fields. My understanding is here you can essentially create a series of things like variables, strings, etc. to refer back to in the methods.
       String next = "------------------------------";
       int a;
       int b;
@@ -16,11 +17,13 @@ public class StudyQuiz {
       int f;
       int g;
 
+      //These are methods... I think... I'm not sure if the scanner utility falls under methods.
       Scanner input = new Scanner(System.in);
 
       System.out.println("Intro to Java Study Quiz");
       System.out.println("Please select your answer by typing in the number corresponding to it");
 
+      //Question 1
       System.out.println("Real-world objects contain " + "_____ " + "and " + "____.");
       System.out.println("1) Constant and Uppercase\n2) Byte and Short\n3) State and Behavior\n4) Postincrement and Preincrement");
       a = input.nextInt();
@@ -37,12 +40,18 @@ public class StudyQuiz {
       {
         System.out.println("Incorrect!");
       }
+
       System.out.println(next);
 
+      //Question 2
       System.out.println("A software object's state is stored in " + "____");
       System.out.println("1) Overflow\n2) Fields\n3) Variables\n4) 64 bit");
       b = input.nextInt();
 
+      if (b > 4)
+      {
+        System.out.println("C'mon now. Did you even read the question?");
+      }
       if (b == 2)
       {
         System.out.println("Correct!");
@@ -52,6 +61,9 @@ public class StudyQuiz {
         System.out.println("Incorrect!");
       }
 
+      System.out.println(next);
+
+      //Question 3
       System.out.println("Hiding internal data from the outside world, and accessing it only through publicly exposed methods is known as data " + "____");
       System.out.println("1) Storage");
       System.out.println("2) Encapsulation");
@@ -59,6 +71,12 @@ public class StudyQuiz {
       System.out.println("4) Corruption");
       d = input.nextInt();
 
+      //tried seperate println statements instead of \n. I think this way looks a little tidier and will probably be easier to edit in the future.
+
+      if (d > 4)
+      {
+        System.out.println("C'mon now. Did you even read the question?");
+      }
       if (d == 2)
       {
         System.out.println("Correct!");
@@ -68,10 +86,17 @@ public class StudyQuiz {
         System.out.println("Incorrect!");
       }
 
+      System.out.println(next);
+
+      //Question 4
       System.out.println("A software object's behavior is exposed through " + "____");
       System.out.println("1) Methods\n2) Fields\n3)Interface \n4) Operators");
       c = input.nextInt();
 
+      if (c > 4)
+      {
+        System.out.println("C'mon now. Did you even read the question?");
+      }
       if (c == 1)
       {
         System.out.println("Correct!");
