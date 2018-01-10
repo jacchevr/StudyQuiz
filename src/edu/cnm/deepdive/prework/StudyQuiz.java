d29f0437c7282a0d7a65ba6e48da44d8e98b46b0
jacchevr <jacchevr@gmail.com>
2018-01-10 02:51:06 -0700

    Added additional questions
    Added additional "if" statement for int used higher than available
    options.
    
    Added string=next to use as a line break between questions.

--- src/edu/cnm/deepdive/prework/StudyQuiz.java
+++ src/edu/cnm/deepdive/prework/StudyQuiz.java
@@ -7,9 +7,14 @@ public class StudyQuiz {
     public static void main(String[] args)
     {
 
+      String next = "------------------------------";
       int a;
       int b;
       int c;
+      int d;
+      int e;
+      int f;
+      int g;
 
       Scanner input = new Scanner(System.in);
 
@@ -20,6 +25,10 @@ public class StudyQuiz {
       System.out.println("1) Constant and Uppercase\n2) Byte and Short\n3) State and Behavior\n4) Postincrement and Preincrement");
       a = input.nextInt();
 
+      if (a > 4)
+      {
+        System.out.println("C'mon now. Did you even read the question?");
+      }
       if (a == 3)
       {
         System.out.println("Correct!");
@@ -28,6 +37,7 @@ public class StudyQuiz {
       {
         System.out.println("Incorrect!");
       }
+      System.out.println(next);
 
       System.out.println("A software object's state is stored in " + "____");
       System.out.println("1) Overflow\n2) Fields\n3) Variables\n4) 64 bit");
@@ -42,7 +52,23 @@ public class StudyQuiz {
         System.out.println("Incorrect!");
       }
 
-      System.out.println("A software object's behavior is stored in " + "____");
+      System.out.println("Hiding internal data from the outside world, and accessing it only through publicly exposed methods is known as data " + "____");
+      System.out.println("1) Storage");
+      System.out.println("2) Encapsulation");
+      System.out.println("3) Allocation");
+      System.out.println("4) Corruption");
+      d = input.nextInt();
+
+      if (d == 2)
+      {
+        System.out.println("Correct!");
+      }
+      else
+      {
+        System.out.println("Incorrect!");
+      }
+
+      System.out.println("A software object's behavior is exposed through " + "____");
       System.out.println("1) Methods\n2) Fields\n3)Interface \n4) Operators");
       c = input.nextInt();
 
